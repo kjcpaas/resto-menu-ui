db82ffa4a31da362373401847fb93ca73ec25ffb
Joy Paas <kjcpaas@gmail.com>
2024-05-12 22:48:50 +0800

    Change GraphQL API in prod

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import App from "./App.tsx";
+import "./index.css";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  // TODO: make it dynamic by environment
+  uri: "https://resto-menu-api-3c61fad65343.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>,
+  </ApolloProvider>
 );
